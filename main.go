c5a54d4426563f24c479dc5dc0ec56395fdaa99a
rorycai <rorycai@tencent.com>
2023-10-08 17:16:07 +0800

    v2

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package testmod
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s!", name)
+func Hi(name string, lang string) (string, error) {
+	switch lang {
+	case "cn":
+		return fmt.Sprintf("你好，%s！", name), nil
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
+
+	}
 }
